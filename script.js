2c056f07cde74fe4d0ab8e4a877a4f76192775d7
Mark Heil <mark.heil1122@gmail.com>
2020-04-28 21:54:48 -0700

    major rehaul on the quiz, redid HTML and CSS, scrapped most of old JS code

--- script.js
+++ script.js
@@ -1,67 +1,65 @@
-let countdown = document.querySelector("countdown");
-let score = document.querySelector("score");
+let startBtn = document.querySelector("#startBtn")
+let startBox = document.querySelector("#startBox")
+let questionDiv = document.getElementById("question");
+let currentQuestion;
+let currentChoices;
+let timeLeft = 0;
+let timer = document.querySelector("#timer");
+
+
 // Hiding text and buttons that will be used for questions and choices
 
-document.getElementById("question").style.display = "none";
-document.getElementById("buttons").style.display = "none";
+document.querySelector("#questionBox").style.display = "none";
 
 // Start button is clicked, start screen is hidden and then quiz display shows up
 
 function startQuiz() {
-    document.getElementById("start").style.display = "none";
-    document.getElementById("question").style.display = "flex";
-    document.getElementById("buttons").style.display = "flex";
-    populate();
+    document.querySelector("#startBox").style.display = "none";
+    document.querySelector("#questionBox").style.display = "flex";
+    quizgame();
 }
 
-function populate() {
-    if (quiz.isEnded()) {
-    }
-    else {
-        //show question
-        let element = document.getElementById("question");
-        element.innerHTML = quiz.getQuestionIndex().text;
+// make a list of objects representing the quiz data
+// remember that when we want to organize data in JS we typically use objects
+let quizQuestions = [
+    {
+        question: "What genre of video games is Final Fantasy?",
+        choices: ["FPS", "RTS", "RPG", "Sports"],
+        answer: "RPG",
+      },
+      {
+        question: "Which Final Fantasy game has the character Cloud Strife?",
+        choices: ["FFI", "FFVII", "FFVIII", "FFXV"],
+        answer: "FFVII"
+      },
+      {
+        question: "What character name is used in most games?",
+        choices: ["Cid", "Terra", "Erdrick", "Bob"],
+        answer: "Cid"
+      },
+      {
+        question: "Final Fantasy III(snes) is really:",
+        choices: ["FFIV", "FFVI", "FFIX", "FFIII"],
+        answer: "FFVI"
+      },
+  ];
+  // keep track of the question we are currently on
 
-        //show choices
-        let choices = quiz.getQuestionIndex().choices;
-        for (let i = 0; i < choices.length; i++) {
-            let element = document.getElementById("choice" + i);
-            element.innerHTML = choices[i];
-            guess("btn" + i, choices[i]);
-        }
+  function quizgame() {
+    if (quizQuestions.length === 0) {
+        endGame();
     }
-};
-
-function guess(id, guess) {
-    let button = document.getElementById(id);
-    button.onclick = function () {
-        quiz.guess(guess);
-        populate();
+    currentQuestion = quizQuestions.shift()
+    console.log(currentQuestion);
+    currentQuestion.textContent = "";
+    document.getElementById("choices").textContent = "";
+    questionDiv.textContent = currentQuestion.question;
+    for (i = 0; i < currentQuestion.choices.length; i++) {
+        const button = document.createElement("button");
+        button.textContent = currentQuestion.choices[i];
+        document.getElementById("choices").appendChild(button);
+        // button.addEventListener("click", verifyAnswer);
     }
+    
 }
-
-let questions = [
-    new Question("What video game genre is Final Fantasy?", ["FPS", "RTS", "RPG", "Sports"], "RPG"),
-    new Question("Which Final Fantasy game has the character Cloud Strife?", ["FFI", "FFVII", "FFVIII", "FFXV"], "FFVII"),
-    new Question("What character name is used in most games?", ["Cid", "Terra", "Erdrick", "Bob"], "Cid"),
-    new Question("Final Fantasy III(snes) is really:", ["FFIV", "FFVI", "FFIX", "FFIII"], "FFVI")
-];
-
-let quiz = new Quiz(questions);
-
-populate();
-
-// function showProgress() {
-//     let currentQuestionNumber = quiz.quesitonIndex + 1;
-//     let element = document.getElementById("progress");
-//     element.innerHTML = "Question " + currentQuestionNumber + "of " + quiz.questions.length;
-
-// }
-
-// function showScores() {
-//     let gameOverHtml = "<h1>Result</h1>";
-//     gameOverHtml += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
-//     let element = document.getElementById("quiz");
-//     element.innerHTML = gameOverHtml;
-// }
 
